ee62bc438a32bf303e35c37ecba4da6e08dbb525
Dakota Johnson <47164434+D7K7@users.noreply.github.com>
2023-12-16 14:00:09 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,61 +1,59 @@
 using System.Text;
 
 public class HumanTimeFormat{
-  public static string formatDuration(int seconds){
-    if (seconds.Equals(0)) return "now";
-    
-    StringBuilder readableFormat = new StringBuilder();
+  public static string CreateMessage(int _years, int _days, int _hours, int _minutes, int _seconds)
+  {
+      StringBuilder message = new StringBuilder();
+      string commaSeparator = ", ";
+  
+      //Years
+      if (!_years.Equals(0)) message.Append(_years + (_years == 1 ? " year" : " years"));
+      //Joining String
+      if (!_years.Equals(0) && !_days.Equals(0)) message.Append(commaSeparator);
+      //Days
+      if (!_days.Equals(0)) message.Append(_days + (_days == 1 ? " day" : " days"));
+      //Joining String
+      if (!_days.Equals(0) && !_hours.Equals(0)) message.Append(commaSeparator);
+      //Hours
+      if (!_hours.Equals(0)) message.Append(_hours + (_hours == 1 ? " hour" : " hours"));
+      //Joining String
+      if (!_hours.Equals(0) && !_minutes.Equals(0)) message.Append(commaSeparator);
+      //Minutes
+      if (!_minutes.Equals(0)) message.Append(_minutes + (_minutes == 1 ? " minute" : " minutes"));
+      //Joining String
+      if (!_minutes.Equals(0) && !_seconds.Equals(0)) message.Append(" and ");
+      //Seconds
+      if (!_seconds.Equals(0)) message.Append(_seconds + (_seconds == 1 ? " second" : " seconds"));
+  
+      return message.ToString();
+  }
 
-    int years = 0, days = 0, hours = 0, minutes = 0;
-
-    //Years
-    if (seconds % 31536000 != 0)
-    {
-        years = seconds / 31536000;
-        seconds = seconds - (years * 31536000);
-
-        if (!years.Equals(0)) readableFormat.Append(years + (years == 1 ? " year" : " years"));
-    }
-
-    //Days
-    if (seconds % 86400 != 0)
-    {
-        if (!years.Equals(0))
-        {
-            readableFormat.Append(", ");
-        }
-
-        days = seconds / 86400;
-        seconds = seconds - (days * 86400);
-        if (!days.Equals(0)) readableFormat.Append(days + (days == 1 ? " day" : " days"));
-    }
-
-    //Hours
-    if (seconds % 3600 != 0)
-    {
-        if (!days.Equals(0))
-        {
-            readableFormat.Append(", ");
-        }
-        hours = seconds / 3600;
-        seconds = seconds - (hours * 3600);
-        if (!days.Equals(0)) readableFormat.Append(hours + (hours == 1 ? " hour" : " hours"));
-    }
-
-    //Minutes
-    if (seconds % 60 != 0)
-    {
-        if (!days.Equals(0))
-        {
-            readableFormat.Append(", ");
-        }
-        minutes = seconds / 60;
-        seconds = seconds - (minutes * 60);
-        if (!minutes.Equals(0)) readableFormat.Append(minutes + (minutes == 1 ? " minute" : " minutes"));
-    }
-
-    if (!minutes.Equals(0)) readableFormat.Append(" and " + seconds + (seconds == 1 ? " second" : " seconds"));
-
-    return readableFormat.ToString();
+  public static string formatDuration(int seconds)
+  {
+      if (seconds.Equals(0)) return "now";
+  
+      int years = 0, days = 0, hours = 0, minutes = 0;
+  
+      //Years
+  
+          years = seconds / 31536000;
+          seconds -= years * 31536000;
+  
+      //Days
+  
+          days = seconds / 86400;
+          seconds -= days * 86400;
+  
+      //Hours
+  
+          hours = seconds / 3600;
+          seconds -= hours * 3600;
+  
+      //Minutes
+  
+          minutes = seconds / 60;
+          seconds -= minutes * 60;
+  
+      return CreateMessage(years,days,hours, minutes, seconds);
   }
 }
